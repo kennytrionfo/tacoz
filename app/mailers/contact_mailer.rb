10f0505a1cc50f33e9b8306f128e9dce618a88fb
kennytrionfo <kentrionfo@gmail.com>
2014-07-10 10:47:06 -0600

    added contact form

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+  default from: "contact-request@tacoz.com"
+
+  def contact_request(params)
+    @params = params
+    mail(to: 'admin@tacoz.com', subject: 'New Contact Request')
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'site#index' #site is controller nad index is the action
   get '/about' => 'site#about'
   get '/contact' => 'site#contact'
+  post '/contact-submit' => 'site#contact_submit', as: :submit_contact
 end
